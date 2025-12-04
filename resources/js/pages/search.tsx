20670cee86edd388d80eb920379f418777975082
Pedro Jefferson <pedro.nsilva@nubank.com.br>
2025-12-04 01:05:32 -0300

    wip

--- resources/js/pages/search.tsx
+++ resources/js/pages/search.tsx
@@ -14,17 +14,17 @@ export default function Search() {
     return (
         <>
             <Header />
-            <div className="w-full xl:w-1/2 mx-auto xl:flex-row flex-col">
-                <div className="w-full xl:w-1/3 bg-white rounded-md flex flex-col xl:m-5 space-y-5 p-6 xl:h-full h-screen">
+            <div className="w-full xl:w-1/2 mx-auto flex xl:flex-row flex-col">
+                <div className="w-full xl:w-1/3 bg-white rounded-md flex flex-col xl:m-5 space-y-5 p-6 xl:h-full h-screen shadow-md shadow-warm-gray">
                     <span>What are you searching for?</span>
                     <div className="flex justify-start space-x-2">
-                        <input type="radio" id="people" name="content" checked={true} /> <label htmlFor={"people"}>People</label>
+                        <input type="radio" id="people" name="content" defaultChecked={true} /> <label htmlFor={"people"}>People</label>
                         <input type="radio" id="movies" name="content" /> <label htmlFor={"movies"}>Movies</label>
                     </div>
                     <input type="text" id="searching" className="w-full h-[50px] border border-black rounded-md" />
-                    <button className="rounded-full bg-default-green text-white uppercase font-bold p-2  bottom-[20px] w-full">Search</button>
+                    <button className="rounded-full bg-default-green text-white uppercase font-bold p-2 self-center bottom-[50px] w-11/12 fixed xl:relative xl:w-full xl:bottom-0">Search</button>
                 </div>
-                <div className="xl:w-2/3 bg-white rounded-md m-5 flex flex-col m-5 space-y-5 p-6">
+                <div className="xl:w-2/3 bg-white rounded-md m-5 flex flex-col m-5 space-y-5 p-6  shadow-md shadow-warm-gray">
                     <h2 className="font-bold text-xl pb-3 border-b border-muted-gray">Results</h2>
                     {empty}
                 </div>
